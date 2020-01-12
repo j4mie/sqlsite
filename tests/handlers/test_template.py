f3084f21828733e3a4fd25fc08352a40c38a738c
Jamie Matthews <jamie@dabapps.com>
2020-01-12 04:03:55 +0000

    Nicer way of embedding templates in test code

--- tests/handlers/test_template.py
+++ tests/handlers/test_template.py
@@ -1,5 +1,6 @@
 from ..utils import create_route, create_sqlar_file
 from sqlsite.wsgi import make_app
+from textwrap import dedent
 
 import httpx
 
@@ -18,21 +19,19 @@ def test_template_handler(db):
 
 def test_template_with_query(db):
     create_route(db, "^$", "template", config="template.html")
-    template = "".join(
-        [
-            "{% with name = sql(\"VALUES('sql')\")[0][0] %}",
-            "<h1>hello {{ name }}</h1>",
-            "{% endwith %}",
-        ]
-    ).encode()
-    create_sqlar_file(db, "template.html", template)
+    template = """
+    {% with name = sql(\"VALUES('sql')\")[0][0] %}
+    <h1>hello {{ name }}</h1>
+    {% endwith %}
+    """
+    create_sqlar_file(db, "template.html", dedent(template).encode())
     app = make_app(db)
     client = httpx.Client(app=app)
     response = client.get("http://test/")
     assert response.status_code == 200
-    assert response.text == "<h1>hello sql</h1>"
+    assert response.text.strip() == "<h1>hello sql</h1>"
     assert response.headers["Content-Type"] == "text/html"
-    assert response.headers["Content-Length"] == "18"
+    assert response.headers["Content-Length"] == "21"
 
 
 def test_missing_template(db):
