62c241d4e6f991050530d54b6fed391585bd67a7
Jamie Matthews <jamie@dabapps.com>
2020-01-14 21:24:51 +0000

    Add test for sql template function when params are passed

--- tests/handlers/test_template.py
+++ tests/handlers/test_template.py
@@ -20,7 +20,7 @@ def test_template_handler(db):
 def test_template_with_query(db):
     create_route(db, "", "template", config="template.html")
     template = """
-    {% with name = sql(\"VALUES('sql')\")[0][0] %}
+    {% with name = sql("VALUES('sql')")[0][0] %}
     <h1>hello {{ name }}</h1>
     {% endwith %}
     """
@@ -30,6 +30,23 @@ def test_template_with_query(db):
     response = client.get("http://test/")
     assert response.status_code == 200
     assert response.text.strip() == "<h1>hello sql</h1>"
+    assert response.headers["Content-Type"] == "text/html"
+    assert response.headers["Content-Length"] == "21"
+
+
+def test_template_with_query_and_params(db):
+    create_route(db, "", "template", config="template.html")
+    template = """
+    {% with name = sql("VALUES(:arg)", params={"arg": "arg"})[0][0] %}
+    <h1>hello {{ name }}</h1>
+    {% endwith %}
+    """
+    create_sqlar_file(db, "template.html", dedent(template).encode())
+    app = make_app(db)
+    client = httpx.Client(app=app)
+    response = client.get("http://test/")
+    assert response.status_code == 200
+    assert response.text.strip() == "<h1>hello arg</h1>"
     assert response.headers["Content-Type"] == "text/html"
     assert response.headers["Content-Length"] == "21"
 
