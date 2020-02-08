dac29b7dcfe10d21228d9c3e1635431f849cb536
Jamie Matthews <jamie@dabapps.com>
2020-02-08 03:48:18 +0000

    Request object now has a .query attribute

--- sqlsite/request.py
+++ sqlsite/request.py
@@ -1,9 +1,13 @@
+from urllib.parse import parse_qs
+
+
 class Request:
-    __slots__ = ["path", "method", "db", "route"]
+    __slots__ = ["path", "method", "query", "db", "route"]
 
     def __init__(self, environ, db):
         self.method = environ["REQUEST_METHOD"].upper()
         self.path = get_str_from_wsgi(environ, "PATH_INFO", "/").replace("/", "", 1)
+        self.query = parse_qs(get_str_from_wsgi(environ, "QUERY_STRING", ""))
         self.db = db
         self.route = None
 
--- tests/test_request.py
+++ tests/test_request.py
@@ -11,3 +11,16 @@ def test_basic_request(db):
     assert request.path == "test/"
     assert request.db is db
     assert request.route is None
+
+
+def test_query_string(db):
+    environ = {
+        "REQUEST_METHOD": "get",
+        "PATH_INFO": "/test/",
+        "QUERY_STRING": "key1=value1&key2=value2",
+    }
+    request = Request(environ, db)
+    assert request.query == {
+        "key1": ["value1"],
+        "key2": ["value2"],
+    }
